708c05f2414431297a3ea9fb2b7278bc083268ec
tgibbons95 <gibbons.thomas95@gmail.com>
2021-09-15 12:32:43 -0500

    Create Baseline For Building, Testing, Installing, and Code Coverage (#2)
    * Add Test Output
    
    * Generate Code Coverage
    
    * Syntax yml
    
    * Install Dependencies
    
    * Install Dependencies
    
    * Install Dependencies
    
    * Install Dependencies
    
    * Install Dependencies
    
    * Install Dependencies
    
    * Remove usr/include from coverage
    
    * Remove usr/include from coverage
    
    * Remove ExternalSource/* from coverage reports
    
    * Archive Installation
    
    * Remove profiling information in release
    
    * Submit code coverage of debug
    
    * Fix windows build
    
    * Fix windows build
    
    * Fix windows build

--- CMake/code_coverage.cmake
+++ CMake/code_coverage.cmake
@@ -2,9 +2,9 @@ function(ENABLE_CODE_COVERAGE TARGET_NAME)
    if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
       # using Clang
    elseif (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-      # using GCC
-      target_compile_options(${TARGET_NAME} PUBLIC -fprofile-arcs -ftest-coverage)
-      target_link_options(${TARGET_NAME} PUBLIC -lgcov --coverage)
+      # using GCC     
+      target_compile_options(${TARGET_NAME} PUBLIC $<$<CONFIG:DEBUG>:-fprofile-arcs> $<$<CONFIG:DEBUG>:-ftest-coverage>)
+      target_link_options(${TARGET_NAME} PUBLIC $<$<CONFIG:DEBUG>:-lgcov> $<$<CONFIG:DEBUG>:--coverage>)
    elseif (CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
       # using Intel C++
    elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
